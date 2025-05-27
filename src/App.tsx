932c95f66bfb6d61c26d679e99bad996dd307664
Ruland <rulandfurqan39@gmail.com>
2025-05-27 20:10:24 +0700

    Added some things

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { WatchHistoryProvider } from "@/contexts/WatchHistoryContext";
 import Index from "./pages/Index";
 import Upload from "./pages/Upload";
@@ -30,6 +31,7 @@ const App = () => (
       </BrowserRouter>
       </WatchHistoryProvider>
     </TooltipProvider>
+    <SpeedInsights />
   </QueryClientProvider>
 );
 
